75432005a54dedae6b0dd671d2d1055d57037a14
Wut <71558901+itswut@users.noreply.github.com>
2023-12-11 07:18:06 +0100

    vehicleTypes missing fix

--- server/sv_functions.lua
+++ server/sv_functions.lua
@@ -1,5 +1,15 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+local vehicleTypes = { -- https://docs.fivem.net/natives/?_0xA273060E
+    motorcycles = 'bike',
+    boats = 'boat',
+    helicopters = 'heli',
+    planes = 'plane',
+    submarines = 'submarine',
+    trailer = 'trailer',
+    train = 'train'
+}
+
 function GetVehicleTypeByModel(model)
     local vehicleData = QBCore.Shared.Vehicles[model]
     if not vehicleData then return 'automobile' end
